313476571742a93da6f517a04f8d96cd1a078382
louisoduor <louisodhiambo20@gmail.com>
2023-12-10 10:31:54 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-function addingEventListener() {
-}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('input');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
+
+  
+  function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+  }
+
+  function c() {
+    alert('I was clicked!');
+  }
+  addingEventListener();
+});
